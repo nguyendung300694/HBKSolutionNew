4acffa7c5a9af10161920da610b780d3daad2a4f
DESKTOP-DEL3KK9\DungNguyen <nguyendung300694@gmail.com>
2018-08-20 08:05:30 +0700

    UPDATE CODE 20/8/2018

--- HBKProject/HBKSolution/HBKSolution/Controllers/ContactController.cs
+++ HBKProject/HBKSolution/HBKSolution/Controllers/ContactController.cs
@@ -5,8 +5,8 @@ using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web;
-using System.Web.Mail;
 using System.Web.Mvc;
+using System.Net.Mail;
 
 namespace HBKSolution.Controllers
 {
@@ -21,24 +21,41 @@ namespace HBKSolution.Controllers
         [HttpPost]
         public ActionResult SendMail(EMailViewModel model)
         {
-            try
+            //try
+            //{
+            //var message = new System.Net.Mail.MailMessage();
+            //message.To.Add(new MailAddress("14110029@student.hcmute.edu.vn"));
+            //message.Subject =  "Khách hàng '" + model.UserName + "' gửi email";
+            //message.Body = "Email của '"+ model.UserName +"': "+ model.UserEmail +" " + "<br/>" +
+            //    "Nội dung: " +model.EmailContent;
+            //message.IsBodyHtml = true;
+            //using (var smtp = new SmtpClient())
+            //{
+            //    smtp.Send(message);
+            //}
+            using (MailMessage mail = new MailMessage())
             {
-                var message = new System.Net.Mail.MailMessage();
-                message.To.Add(new MailAddress("nguyendung300694@gmail.com"));
-                message.Subject =  "Khách hàng '" + model.UserName + "' gửi email";
-                message.Body = "Email của '"+ model.UserName +"': "+ model.UserEmail +" " + "<br/>" +
-                    "Nội dung: " +model.EmailContent;
-                message.IsBodyHtml = true;
-                using (var smtp = new SmtpClient())
+                mail.From = new MailAddress("email@gmail.com");
+                mail.To.Add(new MailAddress("14110029@student.hcmute.edu.vn"));
+                mail.Subject = "Khách hàng '" + model.UserName + "' gửi email";
+                mail.Body = "Email của '" + model.UserName + "': " + model.UserEmail + " " + "<br/>" + "Nội dung: " +model.EmailContent;
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
                 {
-                    smtp.Send(message);
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.Credentials = new System.Net.NetworkCredential("tanlongemailofficial@gmail.com", "A@123456A@");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
                 }
             }
-            catch
-            {
-                return Json(new { success = false });
-            }
             return Json(new { success = true });
+            //}
+            //catch
+            //{
+            //    return Json(new { success = false });
+            //}
         }
     }
 }
